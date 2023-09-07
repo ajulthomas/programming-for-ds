8d0c98591a2d2b0f1f7ec84fbb239bbe0b6edbea
Ajul Thomas <ajul.thomas@outlook.com>
2023-09-07 11:46:59 +1000

    week 5 lab

---
+++ week_5/Week5Tutorial/lists_tuples_practice.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 12:02:58 2023
+
+@author: ajult
+"""
+
+# Question 1: Create a list of 100 elements like this [0, 1, 2, 3, 4, …, 99]
+
+
+
+
+# Question 2: Create a tuple of 100 elements like this (0, 1, 2, 3, 4, …, 99)
+
+
+# •	Question 3: Change values of input_list from string to number and output as output_list
+#       o	input_list = ['2.1', '3.5', '4.8', '1.1', '2.0']
+#       o	output_list = [2.1, 3.5, 4.8, 1.1, 2.0]
+
+
+
+# •	Question 4: Change each element x in a list to x / sum 
+#   where sum is total of all elements in that list.
+#   For example, mylist = [0, 2, 1, 3, 1, 2, 0, 1] 
+#   and sum = 0+2+1+3+1+2+0+1 = 10 
+#   and mylist becomes [0.0, 0.2, 0.1, 0.2, 0.3, 0.2, 0.0, 0.1] 
+
+
+# •	Question 5: Remove the first and last elements from a list. 
+#   For example, my_list = ['red', 0, 2, 1, 1, 2, 0, 1, 'blue'] 
+#   becomes my_list = [0, 2, 1, 1, 2, 0, 1]
+
+
+
+# •	Question 6: Change 0 to 10 in [0, 1, 0, 2, 0, 1] to have [10, 1, 10, 2, 10, 1]
+
+# •	Question 7: Combine list1 and list2 to have list3, list4 and list5 as follows
+#       o	list1 = [2, 3, 1]
+#       o	list2 = [4, 5, 2]
+#       o	list3 = [2, 3, 1, 4, 5, 2]
+#       o	list4 = [[2, 3, 1], [4, 5, 2]]
+#       o	list5 = [(2, 3, 1), (4, 5, 2)]
