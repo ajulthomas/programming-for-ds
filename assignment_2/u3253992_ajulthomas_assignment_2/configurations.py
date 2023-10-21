d52a65aa685a3d6299d430e0099aeecfdd6b8d06
Ajul Thomas <ajul.thomas@outlook.com>
2023-10-21 19:11:40 +1100

    assignment 2 initial commit

---
+++ assignment_2/u3253992_ajulthomas_assignment_2/configurations.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 18:25:11 2023
+
+@author: ajult
+"""
+
---
+++ assignment_2/u3253992_ajulthomas_assignment_2/gui_main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 18:26:23 2023
+
+@author: ajult
+"""
+
+import tkinter as tk
+
+
+class Window(tk.Tk):
+    
+    def __init__(self, title, size):
+        
+        super().__init()
